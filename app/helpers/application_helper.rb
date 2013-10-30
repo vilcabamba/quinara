d553754ee5065e6708e620ed343108ee493463f8
Mario Andrés Correa <admin@macool.me>
2013-10-30 11:28:25 -0500

    added helper for form errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def form_errors_for(object)
+    if object.errors.any?
+      content = pluralize object.errors.count, "error"
+      content += raw "<ul>"
+      object.errors.full_messages.each do |msg|
+        content += raw content_tag :li, msg
+      end
+      content += raw "</ul>"
+      content_tag :div, id: "error_explanation", :class => "alert alert-error" do
+        raw content
+      end
+    end
+  end
+
 end
