711bbf2f9d504e30e03f92f5909834a94c56603e
Mario Andrés Correa <admin@macool.me>
2013-10-30 12:31:46 -0500

    cosillas de mujer

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,12 +1,17 @@
 module Admin
   class UsersController < AdminController
 
+    before_action :find_user, only: [:edit, :update, :destroy]
+
     def index
       @users = User.all
     end
 
     def new
       @user = User.new
+    end
+
+    def edit
     end
 
     def create
@@ -20,7 +25,27 @@ module Admin
       end
     end
 
+    def update
+      if @user.update_attributes user_params
+        flash["alert-success"] = "Usuario actualizado"
+        redirect_to action: :index
+      else
+        flash["alert-error"] = "Usuario no actualizado"
+        render :edit
+      end
+    end
+
+    def destroy
+      @user.destroy
+      flash["alert-info"] = "Usuario eliminado"
+      redirect_to action: :index
+    end
+
     private
+
+    def find_user
+      @user = User.find params[:id]
+    end
 
     def user_params
       params.require(:user).permit :username,
