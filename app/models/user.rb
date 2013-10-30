13aa1194d6d22b5474a35f8b9c8115c42ac58902
Mario Andrés Correa <admin@macool.me>
2013-10-30 10:52:05 -0500

    validations on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
 end
