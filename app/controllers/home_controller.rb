e6ad4f52cec157d3df9280e1c06dd97df9bc7235
Mario Andrés Correa <admin@macool.me>
2013-11-04 12:35:04 -0500

    mostrando lista de cursos a current_user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,10 @@ class HomeController < ApplicationController
   before_action :require_login
 
   def index
-
+    @courses = {
+      teacher: current_user.courses_as_teacher,
+      student: current_user.courses_as_student
+    }
   end
 
 end
