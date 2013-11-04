bd7d1a777c510b53804ccfebe01f91857f8db862
Mario Andrés Correa <admin@macool.me>
2013-11-04 17:26:39 -0500

    agregado #add_student(user) y #remove_student(user) to Course (not tested yet)

--- app/models/concerns/course_users.rb
+++ app/models/concerns/course_users.rb
@@ -6,5 +6,11 @@ module CourseUsers
   def not_students
     @not_students ||= User.where.not id: user_rols.select(:user_id)
   end
+  def add_student(user)
+    user_rols.create rol: Rol.alumno, user: user
+  end
+  def remove_student(user)
+    user_rols.where(rol: Rol.alumno, user: user).destroy
+  end
 
 end
