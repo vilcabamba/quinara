856e6dd9ad5cb1928c688b2b5b3177984fd7a249
Mario Andrés Correa <admin@macool.me>
2013-11-04 13:05:28 -0500

    verificando que un usuario pertenezca a un curso antes de mostrarle el curso

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,24 @@
 class CoursesController < ApplicationController
   
+  before_action :set_course, only: :show
+  before_action :verify_currentuser_belongs_to_course, only: :show
+
   def index
     redirect_to root_path
   end
 
   def show
+  end
+
+  private
+
+  def set_course
     @course = Course.find params[:id]
+  end
+  def verify_currentuser_belongs_to_course
+    unless current_user.belongs_to_course?(@course)
+      redirect_to root_path
+    end
   end
   
 end
