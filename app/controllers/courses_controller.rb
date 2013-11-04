92aede4a619d684b77bd31b1c2a10a1f9b2025a4
Mario Andrés Correa <admin@macool.me>
2013-11-04 16:58:47 -0500

    cleaner implementation

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,6 @@
 class CoursesController < ApplicationController
   
   before_action :set_course, only: :show
-  before_action :verify_currentuser_belongs_to_course, only: :show
 
   def index
     redirect_to root_path
@@ -13,12 +12,7 @@ class CoursesController < ApplicationController
   private
 
   def set_course
-    @course = Course.find params[:id]
-  end
-  def verify_currentuser_belongs_to_course
-    unless current_user.belongs_to_course?(@course)
-      redirect_to root_path
-    end
+    @course = current_user.courses.find params[:id]
   end
   
 end
