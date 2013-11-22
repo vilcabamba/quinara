e1bf6923d6074b1121d6b20fc3dcbf1737017c99
Mario Andrés Correa <admin@macool.me>
2013-11-21 19:53:45 -0500

    botones.

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -14,19 +14,22 @@ module QuestionsHelper
     if question.media_is_image?
       raw "<img src='#{question.media_url}'>
         <small class='legend'>
-          <a href='#{question.media_url}' target='_blank'>Ver imagen en otra ventana</a>
+          <a class='btn btn-mini btn-default' href='#{question.media_url}' target='_blank'>Ver imagen en otra ventana</a>
         </small>
       "
     elsif question.media_is_video?
       raw "
         <video src='#{question.media_url}' controls>Lo sentimos, tu navegador no soporta video. Por favor utiliza otro navegador para poder ver este video</video>
         <small class='legend'>
-          <a href='#{question.media_url}' target='_blank'>Ver video en otra ventana</a>
+          <a class='btn btn-mini btn-default' href='#{question.media_url}' target='_blank'>Ver video en otra ventana</a>
         </small>
       "
     elsif question.media_is_audio?
       raw "
         <audio src='#{question.media_url}' controls>Lo sentimos, tu navegador no soporta audio. Por favor utiliza otro navegador para poder escuchar este audio</audio>
+        <div class='align_center'>
+          <a class='btn btn-mini btn-default' href='#{question.media_url}' target='_blank'>Escuchar archivo de audio</a>
+        </div>
       "
     end
   end
