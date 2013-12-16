b2dc7d55de9b15df14afc9a274e110be6e2cbb31
Mario Andrés Correa <admin@macool.me>
2013-12-16 14:41:14 -0500

    ahora el administrador puede ver las calificaciones de todos los alumnos

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,12 +1,12 @@
 class GradesController < ApplicationController
   
-  def index
-    
-  end
+  before_action :require_login
 
   def show
     if params[:id].to_i == current_user.id
       @user = current_user
+    elsif current_user.admin
+      @user = User.find params[:id]
     else
       redirect_to root_path
       return
