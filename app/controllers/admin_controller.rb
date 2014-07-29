2ca5ca5e4cc279a070b113e074e6695ad776a5ad
Fabricio Flores <fabrifloresg@gmail.com>
2014-07-29 10:18:49 -0500

    Fixing little bug in redirect when not admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,13 +1,14 @@
 class AdminController < ApplicationController
 
   layout "admin"
-
+  before_action :require_login
   before_action :confirm_logged_in_admin
 
   protected
 
   def confirm_logged_in_admin
-    if not require_login or not current_user.admin
+    if not current_user.admin
+      flash["alert-error"] = "No tienes permiso de acceder. Consulta con el administrador del sistema"
       redirect_to root_path
       false
     end
