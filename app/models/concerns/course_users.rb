6ec6ae40d7947f0416a507edcdf16867c201a220
Fabricio Flores <fabrifloresg@gmail.com>
2014-08-11 18:03:56 -0500

    fixing ( reverse commit )

--- app/models/concerns/course_users.rb
+++ app/models/concerns/course_users.rb
@@ -4,7 +4,7 @@ module CourseUsers
     @students ||= User.where id: user_rols.select(:user_id).where(rol_id: Rol.alumno.id)
   end
   def not_students
-    @not_students ||= User.where.not(id: UserRol.select(:user_id)).where(rol_id: Rol.alumno.id)
+    @not_students ||= User.where.not id: UserRol.select(:user_id)
   end
   def add_student(user)
     user_rols.create rol: Rol.alumno, user: user
