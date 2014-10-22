c2f4d2e0e916c75347ab6e34be92818e66992144
Fabricio Flores <fabrifloresg@gmail.com>
2014-10-22 16:47:06 -0500

    add heroku host

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Quinara::Application.initialize!
+
+Rails.application.routes.default_url_options[:host] = 'fte.herokuapp.com'
