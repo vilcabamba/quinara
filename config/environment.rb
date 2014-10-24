d0148d820f072d468426549d59ea2c79a9a791e9
Fabricio Flores <fabrifloresg@gmail.com>
2014-10-24 09:03:42 -0500

    trying a solution for mail

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,13 @@ require File.expand_path('../application', __FILE__)
 Quinara::Application.initialize!
 
 Rails.application.routes.default_url_options[:host] = 'fte.herokuapp.com'
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
