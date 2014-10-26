c06e9748dcd819ea169a0b304df586b781eed451
Fabricio Flores <fabrifloresg@gmail.com>
2014-10-25 23:08:49 -0500

    add from parameter in UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,7 @@
 class UserMailer < ActionMailer::Base
-
   def reset_password_email(user)
     @user = user
     @url  = edit_password_reset_url(user.reset_password_token)
-    mail(:to => user.email, :subject => "Your password has been reset")
+    mail(:from => "FTE",  :to => user.email, :subject => "Your password has been reset")
   end
 end
