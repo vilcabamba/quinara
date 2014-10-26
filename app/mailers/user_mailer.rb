53fabc614c7184dc1c491f73c0d7ea780635285c
Fabricio Flores <fabrifloresg@gmail.com>
2014-10-26 00:03:17 -0500

    Solo cambios de texto

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,9 @@
 class UserMailer < ActionMailer::Base
+  default :from => "FTE"
+  
   def reset_password_email(user)
     @user = user
     @url  = edit_password_reset_url(user.reset_password_token)
-    mail(:from => "FTE",  :to => user.email, :subject => "Your password has been reset")
+    mail(:to => user.email, :subject => "Has solicitado cambiar la contraseña")
   end
 end
