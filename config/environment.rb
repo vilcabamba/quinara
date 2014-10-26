6eff2ae820171b4f7d7f787f912d3237d96ff8cd
Fabricio Flores <fabrifloresg@gmail.com>
2014-10-25 22:23:55 -0500

    dejando los archivos en orden

--- config/environment.rb
+++ config/environment.rb
@@ -3,16 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Quinara::Application.initialize!
-
-Rails.application.routes.default_url_options[:host] = 'fte.herokuapp.com'
-
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'fte.heroku.com',
-  :enable_starttls_auto => true
-}
