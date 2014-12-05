6f383713a162f9fabd564072b3dc1ead4bf2c2a1
Mario Andrés Correa <a@macool.me>
2014-12-04 19:19:53 -0500

    enhances to `require_login` filter
    Fixes #2
    
    - especificar el orden de los filtros dentro de `DocenteController`
    - quitar el `before_action :require_login` que @fabricioflores agregó en bce9bfd1064d49ad2ef822c97d84dbae9cdc9547 (:clap:). Todas estas clases ya heredan el filtro

--- app/controllers/docente_controller.rb
+++ app/controllers/docente_controller.rb
@@ -1,12 +1,11 @@
 class DocenteController < ApplicationController
-  
-  before_action :require_login
-  before_action :set_course
+
+  before_action :require_login, :set_course
 
   protected
 
   def set_course
     @course = current_user.courses_as_teacher.find params[:course_id]
   end
-  
+
 end
--- app/controllers/respuestas_controller.rb
+++ app/controllers/respuestas_controller.rb
@@ -1,5 +1,4 @@
 class RespuestasController < DocenteController
-  before_action :require_login
   before_filter :find_evaluacion
 
   def index
@@ -17,5 +16,5 @@ class RespuestasController < DocenteController
   def find_evaluacion
     @evaluacion = @course.evaluaciones.find params[:evaluacion_id]
   end
-  
+
 end
