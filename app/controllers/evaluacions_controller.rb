9affeffdbde8ad1cdec02829432b660ec180ed8b
Fabricio Flores <fabrifloresg@gmail.com>
2015-01-04 18:35:21 -0500

    setting value to correct columns

--- app/controllers/evaluacions_controller.rb
+++ app/controllers/evaluacions_controller.rb
@@ -53,7 +53,8 @@ class EvaluacionsController < DocenteController
     @user = User.find(params[:user_id])
     params[:answers].each do |answer_id, answer_score|
       @user.user_answers.find_by(id: answer_id, question_id: @evaluacion.questions_ids).tap do |user_answer|
-        user_answer.written_answer_grade = { score: answer_score[:score], text: answer_score[:text] }
+        user_answer.written_answer_score = answer_score[:score]
+        user_answer.written_answer_grade = { text: answer_score[:text] }
         user_answer.save
       end
     end
