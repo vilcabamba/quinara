5ded167a6269c01c0877cdb38c18214c43443b15
Fabricio Flores <fabrifloresg@gmail.com>
2015-01-09 22:29:46 -0500

    fixing little bug

--- app/models/user_answer.rb
+++ app/models/user_answer.rb
@@ -42,11 +42,11 @@ class UserAnswer < ActiveRecord::Base
   end
 
 # methods
-  # def set_score_to_float_if_necessary
-  #   if written_answer_grade
-  #     written_answer_grade[:score] = written_answer_grade[:score].to_f
-  #   end
-  # end
+  def set_score_to_float_if_necessary
+    if written_answer_grade && !written_answer_score
+      written_answer_score = written_answer_grade[:score].to_f
+    end
+  end
   def written_answer_grade_score
     written_answer_score
   end
